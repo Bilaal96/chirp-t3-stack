ad5001305f5cfe0800844b6cb5aa86f65f73f94f
Bilaal Ahmad <bilaal-ahmad@hotmail.co.uk>
2023-07-26 14:30:06 +0100

    Fetch post & author data; create PostView; render data in PostView

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,5 +1,17 @@
+import { clerkClient } from "@clerk/nextjs";
+import type { User } from "@clerk/nextjs/dist/types/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+const filterUserForClient = (user: User) => {
+  const { id, username, profileImageUrl } = user;
+  return {
+    id,
+    username,
+    profileImageUrl,
+  };
+};
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
@@ -9,7 +21,33 @@ export const postRouter = createTRPCRouter({
         greeting: `Hello ${input.text}`,
       };
     }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    // take: limits number of posts returned
+    const posts = await ctx.prisma.post.findMany({ take: 100 });
+
+    // Import clerkClient and use it to get all user info for each post author
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+
+      // Ensure that author is not undefined
+      // eslint-disable-next-line @typescript-eslint/prefer-optional-chain
+      if (!author || !author.username)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for posts not found",
+        });
+
+      return {
+        post,
+        author,
+      };
+    });
   }),
 });
